72ad1484d1a047f5f8576e61ca5e4dc00a7cf049
Orestes Polyzos <orestes.polyzos@gmail.com>
2019-01-19 15:17:32 +0200

    Initial commit

---
+++ src/main/java/com/uoa/di/csr/api/Chicago311ServiceRequestsRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.uoa.di.csr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chicago311ServiceRequestsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chicago311ServiceRequestsRestApiApplication.class, args);
+    }
+
+}
+
